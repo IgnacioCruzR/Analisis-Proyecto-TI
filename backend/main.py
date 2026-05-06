c7d6c6fe448b207837b2ac55cef1e2de3e8de944
Matia <matias.villalobos01@alumnos.ucn.cl>
2026-05-06 09:47:30 -0400

    feat: creación base de datos con postgres y conexión con el backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from config.database import engine, Base, get_db
+
+# Crear tablas en la base de datos
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
